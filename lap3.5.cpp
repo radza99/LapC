8de5179e23e8dfc7a1f9d8c59437ecc41ea10385
radza99 <111630421+radza99@users.noreply.github.com>
2024-01-26 19:36:02 +0700

    Update lap3.5.cpp

--- lap3.5.cpp
+++ lap3.5.cpp
@@ -8,7 +8,7 @@ int main() {
   struct student children[10][10];
   int group;
   GetStudent(children, &group);
-  // แสดงข้อมูลของนักเรียน
+
  for (int i = 0; i < group; i++) {
  for (int j = 0; j < 10; j++) {
 printf("student %d is classroom %d   %s\n", j + 1,  i + 1, children[i][j].name);
